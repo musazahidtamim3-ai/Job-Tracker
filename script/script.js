7cfa7a3fe16a0d020516673b89019a17ca9eca51
musazahidtamim3-ai <musazahidtamim3@gmail.com>
2026-02-24 12:18:29 +0600

    calculate count functionality added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,17 @@
-console.log("hello!")
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("total");
+let intCount = document.getElementById("int-count");
+let rejCount = document.getElementById("rej-count");
+
+
+
+function calculateCount(){
+    total.innerText = cardContainer.children.length;
+    intCount.innerText = interviewList.length;
+    rejCount.innerText = rejectedList.length;
+}
+
+calculateCount();
+
